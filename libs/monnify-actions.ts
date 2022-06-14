920b99454d3a3cf892c419c5c07c324b54a69d65
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2022-06-14 11:27:30 +0100

    remove console.logs

--- libs/monnify-actions.ts
+++ libs/monnify-actions.ts
@@ -1,7 +1,4 @@
 export const callMonnifySDK = (monnifyArgs: Record<string, any>): void => {
-  console.log('callMonnifySDK', monnifyArgs);
   //@ts-ignore
   const handler = window.MonnifySDK && window.MonnifySDK.initialize(monnifyArgs);
-  console.log('handler', window);
-  // handler && handler.loadIframe(monnifyArgs);
 };
